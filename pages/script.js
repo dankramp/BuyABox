67b72f5a54f1dc89b970caf80363d10c78173223
Dan Kramp <danielbkramp@gmail.com>
2018-11-14 16:56:48 -0500

    Added nice animations and made everything more generative

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,37 @@
 "use strict;"
 
+function updateBoard(board_div, teams) {
+	var amt_raised = 0;
+	var board_html = "<div class='row'>";
+	var i;
+	for (i = 0; i < 36; i++) {
+		var box = data['boxes'][i];
+		if (box['bought']) { // box has been bought
+			var t = teams[box['team']];
+			board_html += "<a class='popover-dismiss col-2 purchased-box' role='button' tabindex='0' data-toggle='popover' data-trigger='focus' title='" + box['buyer'] + "' data-content=\"" + box['message'] + "\" style='animation-delay: " + i / 64 + "s; background: " + t['color'] + ";'>$" + box['value'] + "</a>";
+			amt_raised += box['value'];
+			t['amount'] += box['value'];
+
+		} else { // not bought
+			board_html += "<div class='col-2 unpurchased-box' style='animation-delay: " + i / 64 + "s;'>$" + box['value'] + "</div>";
+		}	
+	}
+	board_html += "</div>";
+	board_div.empty();
+	board_div.append(board_html);
+	return amt_raised;
+}
+
+function updateStats(amt_raised, teams) {
+	const total_amt = 666;
+	// Description
+	$('#description-p').empty().append(data['board']['description']);
+	$('#owner-span').empty().append(data['board']['owner']);
+	// Progress bar
+	$('#amt-raised').empty().append("$" + amt_raised);
+	var percentage = Math.floor(amt_raised / total_amt * 100);
+	$('#raised-progress-bar').width(percentage + "%").prop("aria-valuenow", "" + percentage);
+}
 
 var data = {
 	"board": {
@@ -355,48 +387,15 @@ function loadPage() {
 			'amount': 0
 		};
 	}
-	var amt_raised = 0;
-	const total_amt = 666;
 
 	// Make request for data
 
 	// Update viewport with data
-	board_title.html(data['board']['name']);	
-	function updateBoard() {
-		var board_html = "<div class='row'>";
-		var i;
-		for (i = 0; i < 36; i++) {
-			var box = data['boxes'][i];
-		if (box['bought']) { // box has been bought
-			var t = teams[box['team']];
-			board_html += "<a class='popover-dismiss col-2 purchased-box' role='button' tabindex='0' data-toggle='popover' data-trigger='focus' title='" + box['buyer'] + "' data-content=\"" + box['message'] + "\" style='background: " + t['color'] + ";'>$" + box['value'] + "</a>";
-			amt_raised += box['value'];
-			t['amount'] += box['value'];
-
-		} else { // not bought
-			board_html += "<div class='col-2 unpurchased-box'>$" + box['value'] + "</div>";
-		}	
-	}
-	board_html += "</div>";
-	board_div.empty();
-	board_div.append(board_html);	
+	board_title.html(data['board']['name']);
+	var amt_raised = updateBoard(board_div, teams);
 	$('.popover-dismiss').popover({
 		trigger: 'focus',
 		placement: 'auto'
 	});
-}
-
-
-function updateStats() {
-	// Description
-	$('#description-p').empty().append(data['board']['description']);
-	// Progress bar
-	$('#amt-raised').empty().append("$" + amt_raised);
-	var percentage = Math.floor(amt_raised / total_amt * 100);
-	$('#raised-progress-bar').width(percentage + "%").prop("aria-valuenow", "" + percentage);
-}
-	// Update board
-	updateBoard();
-	// Update stats
-	updateStats();
+	updateStats(amt_raised, teams);
 }
