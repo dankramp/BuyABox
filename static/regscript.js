e8d23680c56471d9091b0d2d8fb23996880a7f05
Dan Kramp <danielbkramp@gmail.com>
2018-12-17 16:09:06 -0500

    signup sends post request

--- static/regscript.js
+++ static/regscript.js
@@ -1,18 +1,18 @@
-var btn = $('#reg-btn');
 var pwd = $('#inputPassword');
 var pwd_c = $('#inputPasswordConfirm');
 var err = $('#err-msg');
+var form = $('#signup-form');
 
 function checkpwd() {
     if (pwd.val() != pwd_c.val()) { // passwords don't match
 	err.text("Passwords do not match");
 	err.show();
+	return false;
     } else {
 	err.text("");
 	err.hide();
+	return true;
     }
 }
 
-btn.click(checkpwd);
-
-console.log("script loaded");
+form.submit(checkpwd);
